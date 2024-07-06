7ee5065146777e64efbee6a47cdde1500cd4c622
dsootII <dsootsecond@gmail.com>
2024-07-06 20:10:44 +0530

    deploy att 7 ongoing

--- frontend/bloginlearn-frontend/src/lib/utils.ts
+++ frontend/bloginlearn-frontend/src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BACKEND_URL = "http://127.0.0.1:8000/api/"
+export const BACKEND_URL = "https://journal-ten-jet.vercel.app/api/"
 export const ENDPOINTS = {
   //user
   signup: "signup/",
